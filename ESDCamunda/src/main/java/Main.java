0c97a892efcbd51bb3c76ac819e0a871fe06bd0c
Jonas Heinz <j.heinz@student.fontys.nl>
2018-09-24 15:28:01 +0200

    Dockerfile for ESDCamunda working

---
+++ ESDCamunda/src/main/java/Main.java
@@ -0,0 +1,9 @@
+package main.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Camunda Main class alive");
+    }
+}
