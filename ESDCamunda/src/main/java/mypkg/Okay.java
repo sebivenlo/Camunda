69671602fb8537efa366f673d0a0264b80895fff
Jonas Heinz <j.heinz@student.fontys.nl>
2018-09-26 18:45:16 +0200

    Running Running running!

--- ESDCamunda/src/main/java/mypkg/Okay.java
+++ ESDCamunda/src/main/java/mypkg/Okay.java
@@ -1,16 +1,14 @@
+package mypkg;
 import java.util.logging.Logger;
-
 import org.camunda.bpm.client.*;
 
-public class Main {
-    private final static Logger LOGGER = Logger.getLogger(Main.class.getName());
+public class Okay {
+    private final static Logger LOGGER = Logger.getLogger(Okay.class.getName());
 
     public static void main(String[] args) {
-        System.out.println("In MAIN MAIN MAIN");
         ExternalTaskClient client = ExternalTaskClient.create()
                 .baseUrl("http://localhost:8080/engine-rest")
                 .build();
-
         // subscribe to an external task topic as specified in the process
         client.subscribe("charge-card")
                 .lockDuration(1000) // the default lock duration is 20 seconds, but you can override this
