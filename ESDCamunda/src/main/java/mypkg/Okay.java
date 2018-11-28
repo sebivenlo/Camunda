d47606ca8216d46ab7c2482422582ffbfe9e788b
Jonas Heinz <jonas.a.heinz@gmail.com>
2018-11-28 11:29:40 +0100

    Docker working, readme adjusted

--- ESDCamunda/src/main/java/mypkg/Okay.java
+++ ESDCamunda/src/main/java/mypkg/Okay.java
@@ -7,7 +7,7 @@ public class Okay {
 
     public static void main(String[] args) {
         ExternalTaskClient client = ExternalTaskClient.create()
-                .baseUrl("http://0.0.0.0:8080/engine-rest")
+                .baseUrl("http://172.17.0.1:8080/engine-rest")
                 .build();
         // subscribe to an external task topic as specified in the process
         client.subscribe("charge-card")
