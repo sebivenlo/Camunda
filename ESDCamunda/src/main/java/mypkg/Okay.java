a9d43404bd239219187700a6025c7a7f4b6cc133
Jonas Heinz <jonas.a.heinz@gmail.com>
2018-11-28 11:01:31 +0100

    0.0.0.0 added as local host

--- ESDCamunda/src/main/java/mypkg/Okay.java
+++ ESDCamunda/src/main/java/mypkg/Okay.java
@@ -7,7 +7,7 @@ public class Okay {
 
     public static void main(String[] args) {
         ExternalTaskClient client = ExternalTaskClient.create()
-                .baseUrl("http://localhost:8080/engine-rest")
+                .baseUrl("http://0.0.0.0:8080/engine-rest")
                 .build();
         // subscribe to an external task topic as specified in the process
         client.subscribe("charge-card")
